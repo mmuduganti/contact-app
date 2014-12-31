4b9e73c56931c1318dcbf43b45e14fcc187d55f7
mu3o2l2 <muninder_reddy@gap.com>
2014-12-31 08:50:42 -0800

    Refactoring...

--- service/src/main/java/com/nisum/contact/service/ContactService.java
+++ service/src/main/java/com/nisum/contact/service/ContactService.java
@@ -4,6 +4,7 @@ import com.nisum.contact.domain.ContactRequest;
 import com.nisum.contact.domain.ContactResponse;
 import com.nisum.contact.repository.ContactRepository;
 import lombok.Setter;
+import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,7 +20,7 @@ public class ContactService {
     public ContactResponse saveContact(ContactRequest contactRequest) throws Exception {
         ContactResponse contactResponse = new ContactResponse();
         contactRepository.saveContact(contactRequest);
-        contactResponse.setTs(new Date());
+        contactResponse.setTs(new DateTime());
         return contactResponse;
 
     }
