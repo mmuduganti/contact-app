d9887a0bcb870df74bb1250a18afcfbcc6efe73e
mu3o2l2 <muninder_reddy@gap.com>
2014-12-31 09:09:10 -0800

    Fix save

--- service/src/main/java/com/nisum/contact/repository/ContactRepository.java
+++ service/src/main/java/com/nisum/contact/repository/ContactRepository.java
@@ -5,20 +5,14 @@ import com.nisum.contact.configuration.MybatisMapperConfiguration;
 import com.nisum.contact.domain.ContactRequest;
 import com.nisum.contact.repository.mapper.IContactMapper;
 import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class ContactRepository {
+    @Autowired
+    IContactMapper contactMapper;
     public void saveContact(ContactRequest request) throws Exception {
-        DatabaseConfiguration databaseConfiguration = new DatabaseConfiguration();
-        SqlSession rmsSqlSession = (SqlSession) new MybatisMapperConfiguration().sqlSessionFactory(databaseConfiguration.dataSource());
-
-        if (!(rmsSqlSession.getConfiguration().hasMapper(IContactMapper.class))) {
-            rmsSqlSession.getConfiguration().addMapper(IContactMapper.class);
-        }
-
-
-        IContactMapper contactMapper = rmsSqlSession.getMapper(IContactMapper.class);
         contactMapper.addContact(request.getFirstName(), request.getLastName());
     }
 }
